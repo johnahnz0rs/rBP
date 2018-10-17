7c73c658f79356284f818f9a4e7832ee6f64e8a7
johnahnz0rs <johnahn123@gmail.com>
2018-10-17 15:54:54 -0700

    first commit

---
+++ build/precache-manifest.8d2c7e2f4fcef79524923d5a74028428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5938aad0cda111821e1",
+    "url": "/static/js/main.c5938aad.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c5938aad0cda111821e1",
+    "url": "/static/css/main.0171da2f.chunk.css"
+  },
+  {
+    "revision": "ef579c15e3582c50d0992516638b705f",
+    "url": "/index.html"
+  }
+];
