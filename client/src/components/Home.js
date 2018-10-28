4f3979d7dcd2730d0ca5e8046bd3c651478c0b1e
johnahnz0rs <johnahn123@gmail.com>
2018-10-27 18:02:21 -0700

    finally made a basic daily log page (functionality forthcoming)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,38 +7,81 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            today: ''
+            today: '',
+            logPT: '',
+            tempLogPT: ''
         };
         // declare methods here;
         this.inputHandler = this.inputHandler.bind(this);
     }
 
     componentDidMount() {
-        // console.log('*** today ***', this.today, typeof this.today);
+        this.setState({today: moment().format('dddd, MMMM D, YYYY')});
+
     }
 
     inputHandler = (e) => {
         this.setState({[e.target.id]: e.target.value});
     };
 
-    // printToday() {
-    //     console.log('*** today ***', );
-    // }
 
 
 
 
     render() {
 
-        const today = moment().format('dddd, MMMM D, YYYY');
+        // const today = moment().format('dddd, MMMM D, YYYY');
 
         return (
 
             <React.Fragment>
                 <div className="div-home container-fluid">
 
-                    <h3 className="text-center">{today}</h3>
-                    <button className="btn btn-outline-primary" onClick={() => {console.log('*** today ***', today, typeof today)}}>Print today</button>
+                    <h3 className="text-center">{this.state.today}</h3>
+                    <button className="btn btn-outline-primary" onClick={() => {console.log('*** today ***', this.state.today, typeof this.state.today)}}>Print today</button>
+
+
+
+                    {/*HEALTH*/}
+                    {/*PT Log*/}
+                    <div className="log-pt form-group">
+                        <input className="form-check-input" type="checkbox" value={this.state.tempLogPT} id="defaultCheck1"/>
+                        <label className="form-check-label" htmlFor="defaultCheck1">Did you log your PT today?</label>
+                        <button className="btn btn-outline-success" type="button" data-toggle="collapse" data-target="#collapse-pt-details" area-expanded="false" aria-controls="collapse-pt-details">Add details</button>
+                    </div>
+                    <div className="collapse" id="collapse-pt-details">
+                         <div className="card card-body">
+                             <p>add details about PT</p>
+                         </div>
+                    </div>
+
+                    {/*Food Log*/}
+                    <div className="log-food form-group">
+                        <input className="form-check-input" type="checkbox" value={this.state.tempLogFood} id="tempLogFood"/>
+                        <label className="form-check-label" htmlFor="defaultCheck1">Did you log your Food today?</label>
+                        <button className="btn btn-outline-success" type="button" data-toggle="collapse" data-target="#collapse-food-details" area-expanded="false" aria-controls="collapse-food-details">Add details</button>
+                    </div>
+                    <div className="collapse" id="collapse-food-details">
+                        <div className="card card-body">
+                            <p>add details about Food</p>
+                        </div>
+                    </div>
+
+
+
+
+                    {/*WEALTH*/}
+                    {/*Projects Log*/}
+                    <div className="log-pt form-group">
+                        <input className="form-check-input" type="checkbox" value={this.state.tempLogProjects} id="tempLogProjects"/>
+                        <label className="form-check-label" htmlFor="defaultCheck1">Did you work on any Projects today?</label>
+                        <button className="btn btn-outline-success" type="button" data-toggle="collapse" data-target="#collapse-projects-details" area-expanded="false" aria-controls="collapse-projects-details">Add details</button>
+                    </div>
+                    <div className="collapse" id="collapse-projects-details">
+                        <div className="card card-body">
+                            <p>add details about the Projects you worked on today, sucka!</p>
+                        </div>
+                    </div>
 
 
 
